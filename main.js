c01184b3f93f0b834e53649e2c4c6274211ea04d
TRANHUUTINH <126749282+TRANHUUTINH@users.noreply.github.com>
2023-04-24 10:45:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// function showMessage(){
+//     alert('Xin Chao!');
+// }
+
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML = 'Xin Chao'
+}
+
+// function showMessage(){
+//     console.log('Xin chao');
+// }
+
+
+// function showMessage(){
+//     document.write('xin chao')
+// }
